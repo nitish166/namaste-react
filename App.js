983d740978556c28369d111f9283a76ce7596a09
Nitish Kumar <knitish123@gmail.com>
2024-03-12 22:40:00 +0530

    Swiggy website clone for education pur.

--- App.js
+++ App.js
@@ -1,23 +1,79 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+/**
+ * Header
+ * - Logo
+ * - Nav Items
+ * Body
+ * - Search
+ * - Resturant Container
+ * - Resturant card
+ *   - img
+ *   - Name of res, Star rating, cuisine, develiry time
+ * Footer
+ * - Copy Right
+ * - Links
+ * - Address
+ * - Contact
+ */
 
-const Title = () =>  (
-  <h1 className="head" tabIndex="5">
-    Namaste React Using JXS
-  </h1>
-);
-
-const HeaderComponent = () =>(
-    <div id="container">
-        {Title()}
-        <Title /> 
-        <Title></Title>
-        <h1 className="heading">Namaste React Functional Component</h1>
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
-)
+  );
+};
 
+const ResturantCard = () => {
+  return (
+    <div className="res-card">
+      <img
+      className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/lherq1uaw3tcpxim7msa"
+      />
+      <h3>Meghana Foods</h3>
+      <h4>Briyani, North Indian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="Search">Search</div>
+      <div className="res-container">
+        <ResturantCard />
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeaderComponent/>);
+root.render(<AppLayout />);
